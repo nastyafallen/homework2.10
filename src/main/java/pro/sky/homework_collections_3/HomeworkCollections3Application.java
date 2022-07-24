8d14a7e8ff649597982892ed433c993eb34b4d87
nastyafallen <nastyafallen@rambler.ru>
2022-07-24 17:27:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homework_collections_3/HomeworkCollections3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_collections_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCollections3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkCollections3Application.class, args);
+    }
+
+}
